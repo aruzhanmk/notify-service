7554fbca842c8423c0413f22cc48b00ef98d13d5
aruzhanmk <myrzakhan.aruzhan@gmail.com>
2020-12-06 22:01:05 +0600

    initial commit apache kafka

---
+++ src/main/java/kz/iitu/end/Consumer.java
@@ -0,0 +1,17 @@
+package kz.iitu.end;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class Consumer {
+
+    @KafkaListener(topics = "book_requests", groupId = "group_id")
+    public void consume(BookRequest bookRequest) throws IOException {
+        System.out.println(String.format("#### -> Notify user by email: -> %s",
+                "User " + bookRequest.getUserId() + " purchased book "
+                        + bookRequest.getBook().toString()));
+    }
+}
